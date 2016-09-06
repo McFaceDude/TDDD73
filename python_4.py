0611da3f9c08ff8c38dbd2588d0faf689241f496
mcfacedude <samuel0808@gmail.com>
2016-09-06 20:19:44 +0200

    pascal working, not with list coprehenstions

--- python_4.py
+++ python_4.py
@@ -22,7 +22,42 @@ def distribute(val, element):
 #print(distribute('k', [['o'], [0, 1, 2], ['o','o']]))
 
 #404
-def pascal(n):
-	return [distribute(x, [[] for y in range(n) ]) for x in range(n)]
 
-print(pascal(3))
+def sum_list(element):
+	return [x+element[index+1] for index, x in enumerate(element[:len(element)-1])]
+
+#print(sum_list([1,3,3,1]))
+
+def pascal(n):
+	lists = [[] for x in range(n)]
+	
+	lists[0].append(1)
+	lists[1].append(1)
+	lists[1].append(1)
+	print(lists)
+
+	for index, alist in enumerate(lists[2:],2):
+		alist.append(1)
+		lists[index] = alist + sum_list(lists[index-1])
+		
+		lists[index].append(1)
+		print(lists[index])
+
+
+
+	# lists = distribute(1, lists)
+	# lists[1].append(1)
+	# for i, x in enumerate(lists[2:],2):
+
+	# 	x = [alist + sum_list(lists[index-1]) for index, alist in enumerate(lists[len(lists)-1:], 2)]
+	# 	x.append(1)
+	# 	print(x)
+	# print("")
+	# print(lists[2])
+	# print(sum_list(lists[3]))
+
+	
+	print(lists)
+	
+
+pascal(1)
