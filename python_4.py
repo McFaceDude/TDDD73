17eb2e57955df20cdb431bea484ce020c146d41f
mcfacedude <samuel0808@gmail.com>
2016-09-07 18:57:12 +0200

    done with practice

--- python_4.py
+++ python_4.py
@@ -22,42 +22,17 @@ def distribute(val, element):
 #print(distribute('k', [['o'], [0, 1, 2], ['o','o']]))
 
 #404
-
-def sum_list(element):
-	return [x+element[index+1] for index, x in enumerate(element[:len(element)-1])]
-
-#print(sum_list([1,3,3,1]))
+def pascal_line(n):
+	line = [1]
+	for k in range(n):
+		line.append(int(line[k] * (n-k) / (k+1)))
+	return line
 
 def pascal(n):
-	lists = [[] for x in range(n)]
+	return [pascal_line(i) for i in range(n)]
 	
-	lists[0].append(1)
-	lists[1].append(1)
-	lists[1].append(1)
-	print(lists)
+#print(pascal(7))
 
-	for index, alist in enumerate(lists[2:],2):
-		alist.append(1)
-		lists[index] = alist + sum_list(lists[index-1])
-		
-		lists[index].append(1)
-		print(lists[index])
-
-
-
-	# lists = distribute(1, lists)
-	# lists[1].append(1)
-	# for i, x in enumerate(lists[2:],2):
-
-	# 	x = [alist + sum_list(lists[index-1]) for index, alist in enumerate(lists[len(lists)-1:], 2)]
-	# 	x.append(1)
-	# 	print(x)
-	# print("")
-	# print(lists[2])
-	# print(sum_list(lists[3]))
-
-	
-	print(lists)
-	
-
-pascal(1)
+####
+#4A#
+####
